3767aefc03e3dc6c50541da940a9fe8d0dcecdcf
TriNguyen1208 <ductri0981@gmail.com>
2025-12-16 10:42:24 +0700

    feature: Final project

---
+++ main.py
@@ -0,0 +1,19 @@
+import pickle
+import argparse
+
+model = pickle.load(open('model.pkl', mode='rb'))
+def main():
+    parser = argparse.ArgumentParser(description="Process sensor data: temperature, humidity, air quality (ppm).")
+    parser.add_argument("--temp", type=float, required=True, help="Temperature in Celsius")
+    parser.add_argument("--humid", type=float, required=True, help="Humidity in percentage")
+    parser.add_argument("--ppm", type=float, required=True, help="Air quality (PPM)")
+    args = parser.parse_args()
+
+    temperature = args.temp
+    humidity = args.humid
+    ppm = args.ppm
+
+    pred = model.predict([[temperature, humidity, ppm]])
+    print(pred[0], end='')
+if __name__ == '__main__':
+    main()
